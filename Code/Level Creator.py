74db88b57444deed0d9194914d57b4fb2fa96abf
Lil Pole <andrzejjaniakpalma@gmail.com>
2024-05-02 23:44:24 +0100

    redesigned the prodj to be more big brain

--- Code/Level Creator.py
+++ Code/Level Creator.py
@@ -1,120 +1,74 @@
 ###TODO###
-# - Use 2dArray to store the LevelList
+##fix issue that drawing a circle doesnt get cleared on the surface
 
-
-import pygame, os
+import pygame, os, random
 
 Win_x,Win_y = 1080*0.75,1920*0.75
 Clock = pygame.time.Clock()
-FPS = 60
-SavedLevels = os.listdir("D:\.My folder\Personal stuff\coding stuff\Coding Projects\Medieval Mall\Code\Levels")
-Textures = os.listdir("D:\.My folder\Personal stuff\coding stuff\Coding Projects\Medieval Mall\Assets")
+FPS = 120
 
-#Draws the level using the images in the assets folder
-def DrawLevel(LevelList,Rows,Columns,TileSize,Window):
-    for y in range(Columns):
-        for x in range(Rows):
-            TextureFromLevelList = pygame.image.load(f"D:\.My folder\Personal stuff\coding stuff\Coding Projects\Medieval Mall\Assets\{LevelList[y][x]}")
-            Window.blit(TextureFromLevelList,(x*TileSize,y*TileSize))
-
-
-def CreatingWindow(WindowName):
-    pygame.display.init()
-    Window = pygame.display.set_mode((Win_y,Win_x))
-    pygame.display.set_caption(WindowName)
-    return Window
-
-##TODO
-##Loads a level from the folder "levels"
-def LoadLevel():
-    print("Pick the level you would like:")
-    for x in range(len(SavedLevels)):
-        print(f"{x}. {SavedLevels[x]}")
-    LevelChoice = int(input())
-    print(f"Chosen: {SavedLevels[LevelChoice]}")
-
-    CreatingWindow(SavedLevels[LevelChoice])
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit()
-
-##Creates a new level
-def NewLevel():
-    TileSize = int(input("TileSize: "))
-    MapSize = input("MapSize in the format '1920x1080':  ")
-
-    ##extracts the x and y data
-    for i in range(len(MapSize)):
-        if MapSize[i] == "x":
-            MapSize_x = MapSize[0:i]
-            MapSize_y = MapSize[i+1:len(MapSize)]
+def DrawLines():
     
-    ##Casting int and making it a factor of TileSize
-    MapSize_x = int(MapSize_x)
-    MapSize_y = int(MapSize_y)
-    if MapSize_x % TileSize != 0:
-        MapSize_x += TileSize-(MapSize_x%TileSize)
-    if MapSize_y % TileSize != 0:
-        MapSize_y += TileSize-(MapSize_y%TileSize)
-    print(f"MapSize_x: {MapSize_x}\nMapSize_y: {MapSize_y}\nTileSize: {TileSize}")
+    pass
 
-    ##generates the empty textures for the level
-    Rows , Columns = MapSize_x//TileSize,MapSize_y//TileSize
-    LevelList = [[0 for j in range(Rows)]for i in range(Columns)]
-    switch = True
-    MetaSwitch = True
-    for y in range(Columns):
-        for x in range(Rows):
-            if x==0:
-                if MetaSwitch == True:
-                    MetaSwitch=False
-                    switch = MetaSwitch
-                else:
-                    MetaSwitch = True
-                    switch = MetaSwitch
+def DrawOrigin(surface,origin):
+    pygame.draw.circle(surface,(255,0,0),(origin),10)
 
-            if switch == True:
-                LevelList[y][x] = "Utility\Empty0.png"
-                switch = False
-            else:
-                LevelList[y][x] = "Utility\Empty1.png"
-                switch = True
+def NewLevel_Setup():
+    TileSize = int(input("TileSize: "))
+    print("Preparing Editor")
+    return TileSize
 
-    Window = CreatingWindow("newLevel")
-    while True:
-        Window.fill(pygame.Color("#211522"))
-        DrawLevel(LevelList,Rows,Columns,TileSize,Window)
+def LoadLevel_Setup():
+    print("Pick the level you would like:")
+    pass
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                exit()
+def Panning(event,Origin,panning,offset):
+    if event.type == pygame.MOUSEBUTTONDOWN and event.button==2:
+        panning = True
+        mousepos = pygame.mouse.get_pos()
+        offset = mousepos[0]-Origin[0],mousepos[1]-Origin[1]
 
-            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
-                mousepos = pygame.mouse.get_pos()
-                try:
-                    LevelList[mousepos[1]//TileSize][mousepos[0]//TileSize] = "Utility\Error.png"
-                except IndexError:
-                    pass
+    elif event.type == pygame.MOUSEBUTTONUP and event.button==2:
+        panning = False
 
-            elif event.type == pygame.MOUSEBUTTONDOWN and event.button==2:
-                panning = True
-                print("panning")
-            elif event.type == pygame.MOUSEBUTTONUP and event.button==2:
-                panning = False
-                print("not panning")
-            
-
-        
-        pygame.display.update()
-        Clock.tick(FPS)
+    return panning,offset
 
 
+##--------------------MAIN-----------------------##
 print("1.If you like to load a level")
 print ("2.If you would like to create a new level")
+WindowName = "NewLevel"
 UserChoice = int(input())
 if UserChoice == 1:
-    LoadLevel()
+    LoadLevel_Setup()
 else:
-    NewLevel()
+    TileSize = NewLevel_Setup()
+
+## window init
+pygame.display.init()
+Window = pygame.display.set_mode((Win_y,Win_x),pygame.RESIZABLE)
+pygame.display.set_caption(WindowName)
+
+## run loop
+origin = 0,0
+offset = 0,0
+panning = False
+while True:
+
+    Window.fill((255,255,255))
+    DrawOrigin(Window,origin)
+
+    if panning == True:
+        mousepos = pygame.mouse.get_pos()
+        origin = (mousepos[0]-offset[0]),(mousepos[1]-offset[1])
         
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+        panning,offset = Panning(event,origin,panning,offset)
+
+    print(Clock.get_fps())
+    pygame.display.update()
+    Clock.tick(FPS)
